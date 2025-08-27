f8882714921b37df64e05c9bdfae1d1fe6391e2d
iimomo <epotetoco3@gmail.com>
2025-08-27 15:59:04 +0900

    環境変数の整備

--- src/config/common.ts
+++ src/config/common.ts
@@ -8,9 +8,6 @@ export const AUTH_EMAIL = process.env.AUTH_EMAIL || '';
 export const AUTH_PASSWORD = process.env.AUTH_PASSWORD || '';
 
 export const SCRAPING_USER_AGENT = process.env.SCRAPING_USER_AGENT || '';
-export const SCRAPING_USER_ID = process.env.SCRAPING_USER_ID || '';
-export const SCRAPING_PASSWORD = process.env.SCRAPING_PASSWORD || '';
-export const SCRAPING_GAS_URL = process.env.SCRAPING_GAS_URL || '';
 
 export const LINE_CHANNEL_ACCESS_TOKEN =
   process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
