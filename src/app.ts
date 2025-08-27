8fe91a05490557566211b1e1dd1e8ac68ec614e9
koda-momo <epotetoco3@gmail.com>
2025-08-27 23:12:55 +0900

    認証周りの微調整 (#54)
    * ログイン周り微調整
    
    * セキュリティ周り調整

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
 import cors from 'cors';
 import { apiRouter } from './routes/index';
 import { BASE_URL } from './utils/const';
 import { errorHandler, notFoundHandler } from './middlewares/errorMiddleware';
-
-dotenv.config();
+import { FRONTEND_BASE_URL } from './config/common';
 
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_BASE_URL,
+    origin: FRONTEND_BASE_URL,
     credentials: true,
     optionsSuccessStatus: 200,
   })
