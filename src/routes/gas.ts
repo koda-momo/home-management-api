f2efbcac89a79a16b53d2fb48084ef9f8bbfc3dc
iimomo <epotetoco3@gmail.com>
2025-08-27 15:59:18 +0900

    ガススクレイピングAPIを削除

--- src/routes/gas.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getGasUsage } from '../controllers/gasController';
-
-const router = Router();
-
-router.get('/', getGasUsage);
-
-export { router as gasRouter };
